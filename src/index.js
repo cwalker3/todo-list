48a6228b7ba310b1f77d6240aa0ab7c060a5491e
Cory Walker <cory.walker.dev@gmail.com>
2023-08-05 14:11:28 -0700

    fixed sidebar toggle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { storage } from './storage';
 import { mainController } from './main-controller.js';
 import { sidebarController } from './sidebar-controller.js';
 import { rightPanelController } from './right-panel-controller.js';
+import Close from './sidebar-close.svg';
+import Open from './sidebar-open.svg';
 
 let date = new Date();
 date.toDateString;
